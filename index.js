f5f5ebca5257f414ca2b0817509411546772e2c3
Shukur <shukursurkhayli000@gmail.com>
2024-11-14 13:15:11 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//  Task 1
+let pet = "golden retriever";
+console.log(pet.toUpperCase())
+
+//  Task 2
+let question = "Can you solve this problem?";
+console.log(question.length)
+
+//  Task 3
+let activity = "This game is boring";
+console.log(activity.replace("boring", "exciting"))
+
+// Task 4
+let headline = "Choose Your Adventure!";
+let word = headline.match(/Adventure/)[0];
+console.log(word);
+
+// Task 5
+let desserts = ["Ice Cream", "Pie", "Pudding"]
+desserts.push("Cake")
+console.log(desserts)
+
+//  Task 6
+let cities = ["London", "New York", "Tokyo"];
+console.log(cities.includes("Paris"))
+
+// Task 7 
+let cars = ["Sedan", "Coupe"];
+let trucks = ["Pickup", "SUV"];
+let compare = cars.concat(trucks)
+console.log(compare)
+
+//  Task 8 
+console.log(desserts.indexOf("Pie"))
+
+// Task 9
+let word2 = "Technology"
+console.log(word2.split("").reverse().join(""))
+
+
